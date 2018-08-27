cd05804aa5feb1e098e66660edacc4cabce64995
Lornatang <shiyipaisizuo@gmail.com>
2018-08-27 21:08:59 +0800

    update

---
+++ include/user.h
@@ -0,0 +1,8 @@
+//
+// Created by mac on 2018/8/27.
+//
+
+#ifndef LORNABK_USER_H
+#define LORNABK_USER_H
+
+#endif //LORNABK_USER_H
---
+++ src/user.cc
@@ -0,0 +1,5 @@
+//
+// Created by mac on 2018/8/27.
+//
+
+#include "user.h"
